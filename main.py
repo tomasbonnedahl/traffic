74f61ef903a356bf665388ba7816692b80f7367f
Tomas Bonnedahl <bonnedahl@gmail.com>
2016-03-25 19:52:10 +0100

    Added methods in main

--- main.py
+++ main.py
@@ -10,26 +10,6 @@ north = 59.800000  # Horizontal line
 west =  17.700000  # Vertical line
 east =  18.600000  # Vertical line
 
-def set_box_to_useless(box):
-    box.precision = 5
-    for coordinate in box.coordinates:
-        coordinate.minutes = None
-        coordinate.color = None
-        coordinate.save()
-    box.save()
-
-def set_box_to_4(box):
-    box.precision = 4
-    box.save()
-
-def set_box_precision():
-    # Starts with 1, 2, 8, 9
-    # Ends with 1, 2, 7, 8, 9
-    for box in CoordinateBox.select():
-        index = [int(x) for x in str(box.id)]
-        if len(index) < 2 or index[0] in [1, 2, 6, 7, 8, 9] or index[1] in [0, 1, 2, 7, 8, 9]:
-            set_box_to_useless(box)
-
 def show_all_boxes():
     for box in CoordinateBox.select():
         print box, box.precision
@@ -45,7 +25,7 @@ def create_test_boxes():
     create_boxes_and_coordinates(south, north, west, east, num_boxes_in_dir=10, num_coords_in_dir=15)
 
 def update_box():
-    d = {33: 3, 34: 3, 35: 3, 36: 3, 37 : 3, 47: 3, 57: 3, 74: 2, 75: 2}
+    d = {33: 1, 34: 1, 35: 1, 36: 1, 37 : 1, 47: 1, 52: 1, 53: 1, 57: 1}
     for key, value in d.iteritems():
         box = CoordinateBox.get(CoordinateBox.id == key)
         box.precision = value
@@ -72,7 +52,7 @@ def run():
     print '-- Writing output file'
     write_output_file()
 
-run()
-#show_all_boxes()
-
+#run()
+#update_box()
+show_all_boxes()
 
